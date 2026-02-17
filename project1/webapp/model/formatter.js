83591a03f9157091e70ecad5d266b8cb18d44403
veranikashneidzer <veranika.shneidzer@gmail.com>
2026-02-17 17:46:56 +0200

    Fill the list with data

--- project1/webapp/model/formatter.js
+++ project1/webapp/model/formatter.js
@@ -1,16 +1,41 @@
 sap.ui.define([
-	"sap/ui/core/format/DateFormat",
-	"sap/ui/core/date/UI5Date"
-], (DateFormat, UI5Date) => {
+], () => {
 	"use strict";
 
+	const _getDeliveryStatus = (dRequiredDate, dShippedDate) => {
+		let sDeliveryText = "inTime";
+
+		if (!dShippedDate) {
+			return "";
+		}
+
+		if (dRequiredDate - dShippedDate > 0 && dRequiredDate - dShippedDate <= 7 * 24 * 60 * 60 * 1000) {
+			sDeliveryText = "urgent";
+		} else if (dRequiredDate < dShippedDate) {
+			sDeliveryText = "tooLate";
+		}
+
+		return sDeliveryText;
+	};
+
 	return {
-		formatDate(sDate) {
-			const oDateFormat = DateFormat.getInstance({
-				format: "yyyy"
-			});
-			const sFormattedDate = UI5Date.getInstance(sDate);
-			return oDateFormat.format(sFormattedDate);
+		getDeliveryText(dRequiredDate, dShippedDate) {
+			const oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+			const sDeliveryStatus = _getDeliveryStatus(dRequiredDate, dShippedDate);
+			return oResourceBundle.getText(sDeliveryStatus);
+		},
+
+		getDeliveryState(dRequiredDate, dShippedDate) {
+			const sDeliveryText = _getDeliveryStatus(dRequiredDate, dShippedDate);
+
+			switch (sDeliveryText) {
+				case "urgent":
+					return "Warning";
+				case "tooLate":
+					return "Error";
+				default:
+					return "Success";
+			}
 		}
 	};
 });
