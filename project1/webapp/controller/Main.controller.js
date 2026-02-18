4a383f13fbf0eed4ff9e747e48dd822764d7450f
veranikashneidzer <veranika.shneidzer@gmail.com>
2026-02-18 14:21:55 +0200

    Add filtering and groupping

--- project1/webapp/controller/Main.controller.js
+++ project1/webapp/controller/Main.controller.js
@@ -4,7 +4,15 @@ sap.ui.define([
   "./parts/ODataV2ModelTab",
   "sap/ui/model/Filter",
   "sap/ui/model/FilterOperator",
-], (BaseController, JSONModel, ODataV2ModelTab, Filter, FilterOperator) => {
+  "sap/ui/model/Sorter",
+  "sap/ui/core/format/DateFormat"
+], (BaseController,
+  JSONModel,
+  ODataV2ModelTab,
+  Filter,
+  FilterOperator,
+  Sorter,
+  DateFormat) => {
   "use strict";
 
   return BaseController.extend("project1.controller.Main", Object.assign({}, ODataV2ModelTab, {
@@ -18,6 +26,43 @@ sap.ui.define([
       });
 
       this.getView().setModel(this.configModel, "configModel");
+
+      this.mGroupFunctions = {
+        CustomerName: function (oContext) {
+          const sCustomerName = oContext.getProperty("CustomerName");
+          return {
+            key: sCustomerName,
+            text: sCustomerName
+          };
+        },
+        OrderDate: function (oContext) {
+          const oDate = oContext.getProperty("OrderDate");
+          const iYear = oDate.getFullYear();
+          const iMonth = oDate.getMonth() + 1;
+          const sMonthName = DateFormat.getInstance({ pattern: "MMMM" }).format(oDate);
+          return {
+            key: iYear + "-" + iMonth,
+            text: `Ordered in ${sMonthName} ${iYear}`
+          };
+        },
+        ShippedDate: function (oContext) {
+          const oDate = oContext.getProperty("ShippedDate");
+          if (oDate != null) {
+            const iYear = oDate.getFullYear();
+            const iMonth = oDate.getMonth() + 1;
+            const sMonthName = DateFormat.getInstance({ pattern: "MMMM" }).format(oDate);
+            return {
+              key: iYear + "-" + iMonth,
+              text: `Shipped in ${sMonthName} ${iYear}`
+            };
+          } else {
+            return {
+              key: 0,
+              text: "Not Shipped Yet"
+            };
+          }
+        }
+      };
     },
 
     onOrdersListUpdateFinished() {
@@ -34,5 +79,52 @@ sap.ui.define([
 
       oBinding.filter(aFilters);
     },
-  }))
-});
+
+    async onOpenViewSettings(oEvent) {
+      const sTabId = oEvent.getSource().getId().split("--").pop();
+      try {
+        if (!this.oViewSettingsDialog) {
+          this.oViewSettingsDialog ??= await this.loadFragment({
+            name: "project1.view.fragments.ViewSettingsDialog",
+            id: 'viewSettingsDialog',
+          });
+        }
+
+        this.oViewSettingsDialog.open(sTabId === "groupButton" ? "group" : "filter");
+      } catch (error) {
+        Log.error("Cannot load view settings dialog", error);
+      }
+    },
+
+    onConfirmViewSettingsDialog(oEvent) {
+      const oViewSettingsDialog = this.oViewSettingsDialog;
+      const oList = this.byId("listOfOrders");
+      const oBinding = oList.getBinding("items");
+
+      const sSortPath = oViewSettingsDialog.getSelectedFilterItems()[0]?.getKey() || "";
+      if (sSortPath === "notShippedFilter") {
+        oBinding.filter([new Filter("ShippedDate", FilterOperator.EQ, null)]);
+      } else if (sSortPath === "shippedFilter") {
+        oBinding.filter([new Filter("ShippedDate", FilterOperator.NE, null)]);
+      } else {
+        oBinding.filter([]);
+      }
+
+      const mParams = oEvent.getParameters();
+
+      if (mParams.groupItem) {
+        const aGroups = [];
+        const sPath = mParams.groupItem.getKey();
+        const bDescending = mParams.groupDescending;
+        const vGroup = this.mGroupFunctions[sPath];
+        aGroups.push(new Sorter(sPath, bDescending, vGroup));
+
+        oBinding.sort(aGroups);
+      } else {
+        oBinding.sort([]);
+      }
+    }
+  },
+
+  ))
+});;
